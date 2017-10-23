7867a581b16c70be73d94bc8edf2c309aa1cfdeb
praveen064 <33026968+praveen064@users.noreply.github.com>
2017-10-23 18:10:28 +0530

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,25 @@
+##@Python: Division##
+Q)
+Read two integers and print two lines. The first line should contain integer division, a // b. The second line should contain float division, a / b.
+You don't need to perform any rounding or formatting operations.
+
+Input Format 
+The first line contains the first integer, . The second line contains the second integer, .
+
+Output Format 
+Print the two lines as described above.
+
+Sample Input
+4
+3
+sample Output
+1
+1.3333333333333333
+
+Sol:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(str(a//b), str(a/b))
+    #print("\n".join([str(a//b),str(a/b)]))
