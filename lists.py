9c8bfd9f8519aa189770100d4103291d097567d0
praveen064 <33026968+praveen064@users.noreply.github.com>
2017-10-23 18:32:18 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,126 @@
+##@Lists##   
+=======
+Notes: --->https://www.hackerrank.com/challenges/python-lists/tutorial
+		https://docs.python.org/3/library/stdtypes.html?highlight=split#str.split
+=======
+
+You can initialize a list as:
+arr = list()
+# or simply
+arr = []
+or with a few elements as:
+arr = [1,2,3]
+Elements can be accessed easily similar to most programming languages:
+print arr[0]
+# result is 1
+print arr[0] + arr[1] + arr[2]
+# result is 6
+Lists in Python are very versatile. You can add almost anything in a Python list.
+In Python, you can create a list of any objects: strings, integers, or even lists. You can even add multiple types in a single list!
+Let's look at some of the methods you can use on list.
+1.) append(x)
+Adds a single element x to the end of a list.
+arr.append(9)   
+print arr  
+# prints [1, 2, 3, 9]
+2.) extend(L)
+Merges another list L to the end.
+arr.extend([10,11])
+print arr
+# prints [1, 2, 3, 9, 10, 11]
+3.) insert(i,x) 
+Inserts element x at position i.
+arr.insert(3,7)
+print arr
+# prints [1, 2, 3, 7, 9, 10, 11]
+4.) remove(x) 
+Removes the first occurrence of element x.
+arr.remove(10)  
+arr  
+# prints [1, 2, 3, 7, 9, 11]
+5.) pop() 
+Removes the last element of a list. If an argument is passed, that index item is popped out.
+temp = arr.pop()
+print temp 
+# prints 11
+6.) index(x) 
+Returns the first index of a value in the list. Throws an error if it's not found.
+temp = arr.index(3)
+print temp
+# prints 2
+7.) count(x) 
+Counts the number of occurrences of an element x.
+temp = arr.count(1)
+print temp
+# prints 1
+8.) sort() 
+Sorts the list.
+arr.sort()
+print arr
+# [1, 2, 3, 7, 9]
+9.) reverse() 
+Reverses the list.
+arr.reverse()
+print arr
+# [9, 7, 3, 2, 1]
+
+Consider a list (list = []). You can perform the following commands:
+
+insert i e: Insert integer  at position .
+print: Print the list.
+remove e: Delete the first occurrence of integer .
+append e: Insert integer  at the end of the list.
+sort: Sort the list.
+pop: Pop the last element from the list.
+reverse: Reverse the list.
+
+
+Q) Task:
+Input Format
+
+The first line contains an integer, , denoting the number of commands. 
+Each line  of the  subsequent lines contains one of the commands described above.
+
+Output Format
+
+For each command of type print, print the list on a new line.
+
+Sample Input
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print 
+remove 6
+append 9
+append 1
+sort 
+print
+pop
+reverse
+print
+Sample Output
+
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+
+Sol: 
+if __name__ == '__main__':
+    T = int(input().strip())
+L = []
+for t in range(T):
+    args = input().strip().split(" ")
+    if args[0] == "print":
+        print (L)
+    elif len(args) == 3:
+        getattr(L, args[0])(int(args[1]), int(args[2]))
+    elif len(args) == 2:
+        getattr(L, args[0])(int(args[1]))
+    else:
+        getattr(L, args[0])()
+=====		
+Note:
+===== 
+Using underscore ("_") as the variable name in the for appears to be common practice when you don't intend to use the variable in the loop. Underscore is a valid variable name, so you could use it in the loop, but it would make for unreadable code (what does underscore mean?) so using it designates that the variable has no meaning or is otherwise unnecessary.
